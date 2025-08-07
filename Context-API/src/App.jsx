45569181fffd6e6f89d225d0786e6248663a9c0a
nitishRudyy <nitishkumarrampal@gmail.com>
2025-08-07 14:46:17 +0530

    context done

--- Context-API/src/App.jsx
+++ Context-API/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <h1>Hello CodeSandbox</h1>
+    <h1 className='bg-amber-200'>Hello CodeSandbox</h1>
     <h2>Start editing to see some magic happen!</h2>
     </>
   )
